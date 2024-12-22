42f0457527191b9c31490edf6b250ddf0d4fe236
James Gunn <james@gunn.io>
2024-12-22 14:50:46 +0000

    Add new set of types for creating a component

--- src/GovUk.Frontend.AspNetCore/ComponentGeneration/StringExtensions.cs
+++ src/GovUk.Frontend.AspNetCore/ComponentGeneration/StringExtensions.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Html;
+
 namespace GovUk.Frontend.AspNetCore.ComponentGeneration;
 
 internal static class StringExtensions
@@ -5,4 +7,7 @@ internal static class StringExtensions
     public static string? NormalizeEmptyString(this string? value) => string.IsNullOrEmpty(value) ? null : value;
 
     public static string Capitalize(this string str) => str[0..1].ToUpper() + str[1..];
+
+    public static IHtmlContent? NormalizeEmptyString(this IHtmlContent? htmlString) =>
+        string.IsNullOrEmpty(htmlString?.ToHtmlString()) ? null : htmlString;
 }
